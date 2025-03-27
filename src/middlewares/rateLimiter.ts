7e763ba33f4385586ed8bfaed662666ee91e8a14
Gal Omer <galomer6708@gmail.com>
2025-03-27 14:24:13 +0200

    rate limiter adj

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -6,6 +6,6 @@ import rateLimit from "express-rate-limit";
  */
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per window
+  max: 500, // temporarily allow up to 500 requests per window
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
